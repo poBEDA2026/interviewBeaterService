a8602610abb94736ca3fcadc37bc115cc9a2d455
Polupanova Alina <apolupanova@naumen.ru>
2026-08-31 20:58:00 +0500

    feat (Spring): Первичная настройка проекта и спринга

---
+++ src/main/java/com/github/interviewbeaterservice/InterviewBeaterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.interviewbeaterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBeaterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewBeaterServiceApplication.class, args);
+    }
+}
