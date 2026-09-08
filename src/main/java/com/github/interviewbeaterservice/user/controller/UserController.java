9763850e69ab539206d6355ac6d720659ca4b94a
Polupanova Alina <apolupanova@naumen.ru>
2026-09-08 14:25:10 +0500

    feat (Registration): Добавила возможность регистрации пользователей

---
+++ src/main/java/com/github/interviewbeaterservice/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.github.interviewbeaterservice.user.controller;
+
+import com.github.interviewbeaterservice.user.dto.RegisterRequest;
+import com.github.interviewbeaterservice.user.entity.User;
+import com.github.interviewbeaterservice.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody @Valid RegisterRequest requestBody) {
+        User createdUser = userService.register(requestBody.email(), requestBody.password());
+
+        return createdUser.getId().toString();
+    }
+}
